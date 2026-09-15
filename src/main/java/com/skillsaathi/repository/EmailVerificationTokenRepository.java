1c1bd5651f21d921de1e423a13ca26dfd18f4f59
Raja995501 <153234835+Raja995501@users.noreply.github.com>
2026-09-15 19:46:02 +0530

    Add deleteByUser method to EmailVerificationTokenRepository
    Added a method to delete EmailVerificationToken by User object.

--- src/main/java/com/skillsaathi/repository/EmailVerificationTokenRepository.java
+++ src/main/java/com/skillsaathi/repository/EmailVerificationTokenRepository.java
@@ -1,6 +1,7 @@
 package com.skillsaathi.repository;
 
 import com.skillsaathi.entity.EmailVerificationToken;
+import com.skillsaathi.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -8,4 +9,7 @@ import java.util.Optional;
 public interface EmailVerificationTokenRepository extends JpaRepository<EmailVerificationToken, Long> {
     Optional<EmailVerificationToken> findByToken(String token);
     void deleteByUserId(Long userId);
+
+    // Agar User object pass karna chahte hain toh ye bhi rakh sakte hain:
+    void deleteByUser(User user);
 }
