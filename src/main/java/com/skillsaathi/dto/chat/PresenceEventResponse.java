81cfff1e3c03fc9e1f3b515d143925e0d286e7fe
Raja995501 <153234835+Raja995501@users.noreply.github.com>
2026-09-16 09:06:05 +0530

    Add JSON properties to PresenceEventResponse fields

--- src/main/java/com/skillsaathi/dto/chat/PresenceEventResponse.java
+++ src/main/java/com/skillsaathi/dto/chat/PresenceEventResponse.java
@@ -1,9 +1,12 @@
 package com.skillsaathi.dto.chat;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
 public class PresenceEventResponse {
+    @JsonProperty("userId")
     private Long userId;
+    @JsonProperty("online")
     private boolean online;
 }
